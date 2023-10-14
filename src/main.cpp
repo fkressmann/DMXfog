d01ef463ab9bf15c2f0953784f45d4b6d2b14220
Felix Kressmann <felix@felixkr.de>
2023-10-14 23:38:57 +0200

    Fix display / settings

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,16 @@
 #include <Arduino.h>
 #include <EEPROM.h>
-#include <DMXSerial.h>
+//#include <DMXSerial.h>
 #include <TM1637Display.h>
 
-#define DISPLAY_CLK 2
-#define DISPLAY_DIO 3
-#define SETTINGS_BUTTON 2 // Pin 2 and 3 are usable for interrupts
+#define DISPLAY_CLK 6
+#define DISPLAY_DIO 5
+#define SETTINGS_BUTTON 4 // Pin 2 and 3 are usable for interrupts
 #define UP_BUTTON 2
 #define DOWN_BUTTON 3
-#define DISPLAY_DOT 0b01000000
 #define TRIGGER 10
 
-int dmxStartChannel;
+int dmxStartChannel = 0;
 int dmxDutyCycleChannel;
 int dmxFrequencyChannel;
 int currentFrequencySek = 0; // Interval length, seconds, 0-255
@@ -24,7 +23,13 @@ bool needToTurnOff = false;
 bool upButtonPressed = false;
 bool downButtonPressed = false;
 
+const uint8_t SEG_EDIT[] = {SEG_A | SEG_D | SEG_E | SEG_F | SEG_G};
+const uint8_t SEG_FOG[] = {SEG_A  | SEG_E | SEG_F | SEG_G};
+const uint8_t EMPTY[] = {0};
+
 TM1637Display display(DISPLAY_CLK, DISPLAY_DIO);
+
+void pwm();
 
 void updateChannels() {
     dmxFrequencyChannel = dmxStartChannel;
@@ -40,80 +45,93 @@ void isrDown() {
 }
 
 void setup() {
+    display.setBrightness(0x0f);
     pinMode(SETTINGS_BUTTON, INPUT_PULLUP);
     pinMode(UP_BUTTON, INPUT_PULLUP);
     pinMode(DOWN_BUTTON, INPUT_PULLUP);
     attachInterrupt(digitalPinToInterrupt(UP_BUTTON), isrUp, CHANGE);
     attachInterrupt(digitalPinToInterrupt(DOWN_BUTTON), isrDown, CHANGE);
+    Serial.begin(115200);
+    Serial.println("Starting");
 
-
-    DMXSerial.init(DMXReceiver);
+//    DMXSerial.init(DMXReceiver);
     pinMode(TRIGGER, OUTPUT);
-    // EEPROM.get(0, dmxStartChannel);
-    // Put dmxStartChannel onto display, too
-    dmxStartChannel = 1;
+    EEPROM.get(0, dmxStartChannel);
+    Serial.println(dmxStartChannel);
+    delay(2000);
+    display.showNumberDec(dmxStartChannel);
     updateChannels();
 }
 
 void editSettings() {
+    delay(100);
+    while(!digitalRead(SETTINGS_BUTTON)); // Prevent exiting this menu before settings button is released
+    display.setSegments(SEG_EDIT, 1, 0);
+    Serial.println("Settings");
     int currentInput = dmxStartChannel; // init with currently set dmx channel
     float multiplier = 1;
-    bool changed = true;
+    bool changed = false;
     while (digitalRead(SETTINGS_BUTTON)) {
         if (currentInput > 512) currentInput = 1;
-        if (changed) display.showNumberDecEx(currentInput, DISPLAY_DOT);
+        if (currentInput < 1) currentInput = 512;
+        if (changed) display.showNumberDec(currentInput, false, 3, 1);
         if (upButtonPressed) {
             currentInput += multiplier;
-            multiplier += 0.1;
+            multiplier += 0.3;
             changed = true;
         } else if (downButtonPressed) {
             currentInput -= multiplier;
-            multiplier += 0.1;
+            multiplier += 0.3;
             changed = true;
         } else {
-            multiplier = 0;
+            multiplier = 1;
             changed = false;
         }
-        delay(50);
+        delay(75);
     }
     dmxStartChannel = currentInput;
+    Serial.println(dmxStartChannel);
     EEPROM.put(0, dmxStartChannel);
     updateChannels();
-    display.showNumberDecEx(dmxStartChannel, DISPLAY_DOT);
+    display.showNumberDec(dmxStartChannel);
     while(!digitalRead(SETTINGS_BUTTON)); // Prevent exiting this menu before settings button is released
+    delay(100);
 }
 
 void loop() {
-    if (digitalRead(UP_BUTTON) == LOW) editSettings();
-  unsigned long lastPacket = DMXSerial.noDataSince();
-  if (lastPacket < 5000) {
-    // Check for frequency changes
-    unsigned int newDmxValueFrequency = DMXSerial.read(dmxFrequencyChannel);
-    if (newDmxValueFrequency != currentFrequencySek) {
-      // If interval length changed, reset current running interval and start over
-      currentCycleStart = 0;
-      nextTurnOffTime = 0;
-    }
-
-    // Check for duty cycle changes
-    unsigned int newDmxValueDutyCycle = DMXSerial.read(dmxDutyCycleChannel);
-    if (newDmxValueDutyCycle != currentDutyCycleDmxValue) {
-      currentDutyCycleDmxValue = newDmxValueDutyCycle;
-      currentDutyCycle = (float) newDmxValueDutyCycle / 255;
-    }
-  }
-  
-  unsigned long currentMillis = millis();
-  // Check if new cycle needs to be started
-  if (currentMillis > currentCycleStart) {
-    digitalWrite(TRIGGER, HIGH);
-    needToTurnOff = true;
-    nextTurnOffTime = currentMillis + (currentDutyCycle * currentFrequencySek);
-  }
-  // Check if trigger needs to be tunred off
-  if (needToTurnOff && currentMillis > nextTurnOffTime) {
-    digitalWrite(TRIGGER, LOW);
-    needToTurnOff = false;
-  }
-
+    if (digitalRead(SETTINGS_BUTTON) == LOW) editSettings();
+//    pwm();
 }
+
+//void pwm() {
+//    unsigned long lastPacket = DMXSerial.noDataSince();
+//    if (lastPacket < 5000) {
+//      // Check for frequency changes
+//      unsigned int newDmxValueFrequency = DMXSerial.read(dmxFrequencyChannel);
+//      if (newDmxValueFrequency != currentFrequencySek) {
+//        // If interval length changed, reset current running interval and start over
+//        currentCycleStart = 0;
+//        nextTurnOffTime = 0;
+//      }
+//
+//      // Check for duty cycle changes
+//      unsigned int newDmxValueDutyCycle = DMXSerial.read(dmxDutyCycleChannel);
+//      if (newDmxValueDutyCycle != currentDutyCycleDmxValue) {
+//        currentDutyCycleDmxValue = newDmxValueDutyCycle;
+//        currentDutyCycle = (float) newDmxValueDutyCycle / 255;
+//      }
+//    }
+//
+//    unsigned long currentMillis = millis();
+//    // Check if new cycle needs to be started
+//    if (currentMillis > currentCycleStart) {
+//      digitalWrite(TRIGGER, HIGH);
+//      needToTurnOff = true;
+//      nextTurnOffTime = currentMillis + (currentDutyCycle * currentFrequencySek);
+//    }
+//    // Check if trigger needs to be tunred off
+//    if (needToTurnOff && currentMillis > nextTurnOffTime) {
+//      digitalWrite(TRIGGER, LOW);
+//      needToTurnOff = false;
+//    }
+//}
